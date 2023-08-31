80eaea306ea1d21fab8f8734df469ac8eb5d2b27
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-08-31 18:58:37 +0900

    deleted unused file
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- .github/workflows/regex.sh
+++
@@ -1,6 +0,0 @@
-
-
-echo "[major] kdalfsfk[aaaaaaa]" | sed -n -E 's:^\[(major|minor|patch)\].*:\1:p'
-echo "[minor] kdalfsfk[aaaaaaa]" | sed -n -E 's:^\[(major|minor|patch)\].*:\1:p'
-echo "[patch] kdalfsfk[aaaaaaa]" | sed -n -E 's:^\[(major|minor|patch)\].*:\1:p'
-echo "[ci] kdalfsfk[aaaaaaa]"    | sed -n -E 's:^\[(major|minor|patch)\].*:\1:p'
