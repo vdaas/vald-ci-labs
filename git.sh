50f2d0f13281d452bb07ab57bb26c774415bc3d1
Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
2023-09-15 00:30:33 +0900

    add workflow to release for major/minor/patch (#78)
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- git.sh
+++ git.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 
-var="v1.7.6"
-IFS=. read -r version minor patch <<EOF
-$var
-EOF
+BRANCH_LATEST_TAG="v1.7.6"
 
-case "$1" in
-patch) tag="$version.$minor.$((patch+1))"; ;;
-major) tag="$((version+1)).0.0"; ;;
-*)     tag="$version.$((minor+1)).0"; ;;
+RELEASE_KIND="minor"
+
+IFS=. read -r version minor patch <<< `echo "${BRANCH_LATEST_TAG}" | sed -e 's:^v::'`
+
+case ${RELEASE_KIND} in
+major) RELEASE_TAG="v$((version+1)).0.0";      RELEASE_BRANCH_NAME="v$((version+1)).0";       ;;
+minor) RELEASE_TAG="v$version.$((minor+1)).0"; RELEASE_BRANCH_NAME="v$version.$((minor+1))";  ;;
+*) echo "not supported semver kind"; exit 1; ;;
 esac
 
-echo $tag
+# case "$1" in
+# patch) tag="$version.$minor.$((patch+1))"; ;;
+# minor) tag="$version.$((minor+1)).0"; ;;
+# major) tag="$((version+1)).0.0"; ;;
+# *)     tag="$version.$((minor+1)).0"; ;;
+# esac
+
+echo "PREV_TAG=${BRANCH_LATEST_TAG}"
+echo "RELEASE_KIND=${RELEASE_KIND}"
+echo "${RELEASE_TAG}"
+echo "${RELEASE_BRANCH_NAME}"
+
+
+
+# case "$1" in
+# patch) tag="$version.$minor.$((patch+1))"; ;;
+# major) tag="$((version+1)).0.0"; ;;
+# *)     tag="$version.$((minor+1)).0"; ;;
+# esac
+#
+# echo $tag
