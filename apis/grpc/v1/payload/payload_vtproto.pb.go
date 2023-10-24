a09279eca4630f4774411a99356eb22df7aa289f
Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
2023-10-24 16:55:06 +0900

    fix: delete flag (#137)
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- apis/grpc/v1/payload/payload_vtproto.pb.go
+++ apis/grpc/v1/payload/payload_vtproto.pb.go
@@ -185,7 +185,6 @@ func (m *Search_Config) CloneVT() *Search_Config {
 		EgressFilters:        m.EgressFilters.CloneVT(),
 		MinNum:               m.MinNum,
 		AggregationAlgorithm: m.AggregationAlgorithm,
-		TestFlag:             m.TestFlag,
 	}
 	if len(m.unknownFields) > 0 {
 		r.unknownFields = make([]byte, len(m.unknownFields))
@@ -1898,9 +1897,6 @@ func (this *Search_Config) EqualVT(that *Search_Config) bool {
 		return false
 	}
 	if this.AggregationAlgorithm != that.AggregationAlgorithm {
-		return false
-	}
-	if this.TestFlag != that.TestFlag {
 		return false
 	}
 	return string(this.unknownFields) == string(that.unknownFields)
@@ -4236,16 +4232,6 @@ func (m *Search_Config) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 	if m.unknownFields != nil {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
-	}
-	if m.TestFlag {
-		i--
-		if m.TestFlag {
-			dAtA[i] = 1
-		} else {
-			dAtA[i] = 0
-		}
-		i--
-		dAtA[i] = 0x50
 	}
 	if m.AggregationAlgorithm != 0 {
 		i = encodeVarint(dAtA, i, uint64(m.AggregationAlgorithm))
@@ -7896,9 +7882,6 @@ func (m *Search_Config) SizeVT() (n int) {
 	if m.AggregationAlgorithm != 0 {
 		n += 1 + sov(uint64(m.AggregationAlgorithm))
 	}
-	if m.TestFlag {
-		n += 2
-	}
 	n += len(m.unknownFields)
 	return n
 }
@@ -10076,26 +10059,6 @@ func (m *Search_Config) UnmarshalVT(dAtA []byte) error {
 					break
 				}
 			}
-		case 10:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field TestFlag", wireType)
-			}
-			var v int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflow
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				v |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			m.TestFlag = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := skip(dAtA[iNdEx:])
