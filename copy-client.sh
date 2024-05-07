cf85083fa1cb238efdd7971a91dd59c60384abf6
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2024-05-07 11:40:05 +0900

    feat: copy from vald-client-node
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- copy-client.sh
+++ copy-client.sh
@@ -15,20 +15,42 @@
 #
 
 # Java
-cp -R ../vald-client-java/buf.gen.yaml .
-cp -R ../vald-client-java/buf.work.yaml .
-cp -R ../vald-client-java/build.gradle .
-cp -R ../vald-client-java/examples .
-cp -R ../vald-client-java/.fossa.yml .
-cp -R ../vald-client-java/.github .
-cp -R ../vald-client-java/.gitignore .
-cp -R ../vald-client-java/gradle .
-cp -R ../vald-client-java/gradlew .
-cp -R ../vald-client-java/gradlew.bat .
-cp -R ../vald-client-java/LICENSE .
-cp -R ../vald-client-java/Makefile .
-cp -R ../vald-client-java/README.md .
-cp -R ../vald-client-java/settings.gradle .
-cp -R ../vald-client-java/src .
-cp -R ../vald-client-java/VALD_SHA .
-cp -R ../vald-client-java/version .
+# cp -R ../vald-client-java/buf.gen.yaml .
+# cp -R ../vald-client-java/buf.work.yaml .
+# cp -R ../vald-client-java/build.gradle .
+# cp -R ../vald-client-java/examples .
+# cp -R ../vald-client-java/.fossa.yml .
+# cp -R ../vald-client-java/.github .
+# cp -R ../vald-client-java/.gitignore .
+# cp -R ../vald-client-java/gradle .
+# cp -R ../vald-client-java/gradlew .
+# cp -R ../vald-client-java/gradlew.bat .
+# cp -R ../vald-client-java/LICENSE .
+# cp -R ../vald-client-java/Makefile .
+# cp -R ../vald-client-java/README.md .
+# cp -R ../vald-client-java/settings.gradle .
+# cp -R ../vald-client-java/src .
+# cp -R ../vald-client-java/VALD_SHA .
+# cp -R ../vald-client-java/version .
+
+# Node
+cp -R ../vald-client-node/buf.gen.yaml .
+cp -R ../vald-client-node/buf.work.yaml .
+cp -R ../vald-client-node/example .
+cp -R ../vald-client-node/example-ts .
+cp -R ../vald-client-node/.gitignore .
+cp -R ../vald-client-node/index.d.ts .
+cp -R ../vald-client-node/index.js .
+cp -R ../vald-client-node/jest.config.ts .
+cp -R ../vald-client-node/Makefile .
+cp -R ../vald-client-node/node_modules .
+cp -R ../vald-client-node/.npmignore .
+cp -R ../vald-client-node/.npmrc .
+cp -R ../vald-client-node/package.json .
+cp -R ../vald-client-node/package-lock.json .
+cp -R ../vald-client-node/README.md .
+cp -R ../vald-client-node/src .
+cp -R ../vald-client-node/tests .
+cp -R ../vald-client-node/tsconfig.json .
+cp -R ../vald-client-node/VALD_CLIENT_NODE_VERSION .
+cp -R ../vald-client-node/VALD_SHA .
